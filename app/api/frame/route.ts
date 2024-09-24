b010016663459cf4e16c0b1d2ae18253b2330e7a
Mark Krasner <mzkrasner@gmail.com>
2024-09-24 17:21:23 -0400

    Fixing linear gradient to black and white for quiz questions

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -64,8 +64,8 @@ const generateSVG = (question: string, answers?: string[]) => {
     <svg width="${svgWidth}" height="600" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <linearGradient id="bg-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" style="stop-color:#2c3e50;stop-opacity:1" />
-          <stop offset="100%" style="stop-color:#1a2530;stop-opacity:1" />
+          <stop offset="0%" style="stop-color:#1D1F1F;stop-opacity:1" />
+          <stop offset="100%" style="stop-color:#000000;stop-opacity:1" />
         </linearGradient>
       </defs>
       <rect width="100%" height="100%" fill="url(#bg-gradient)" rx="15" ry="15"/>
